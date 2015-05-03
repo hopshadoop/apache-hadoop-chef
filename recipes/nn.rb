96fe8426322db58a33c2cbb72b45f466cfa8581e
Jim Dowling <jdowling@sics.se>
2015-05-03 07:04:24 +0200

    bugfix

--- recipes/nn.rb
+++ recipes/nn.rb
@@ -23,7 +23,7 @@ if node[:hadoop][:format].eql? "true"
     # if the nn has already been formatted, re-formatting it returns error
     Chef::Log.info "Not formatting the NameNode. Remove this directory before formatting: (sudo rm -rf #{node[:hadoop][:tmp_dir]}/dfs/name/current)"
   else 
-    hops_start "format-nn" do
+    hadoop_start "format-nn" do
       action :format_nn
     end
   end
