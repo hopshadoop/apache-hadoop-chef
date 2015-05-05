d76f7bcbdf09a5dbf738465b62e45fccbaea3715
Jim Dowling <jdowling@kth.se>
2015-05-05 13:24:01 +0200

    more updates

--- providers/hdfs_directory.rb
+++ providers/hdfs_directory.rb
@@ -3,12 +3,12 @@ action :create do
 
   bash "mk-dir-#{new_resource.name}" do
     user #{new_resource.owner}
+    group #{new_resource.group} 
     code <<-EOF
      set -e
      . #{node[:hadoop][:home]}/sbin/set-env.sh
      #{node[:hadoop][:home]}/bin/hdfs dfs -mkdir -p #{new_resource.name}
      #{node[:hadoop][:home]}/bin/hadoop fs -chmod #{new_resource.mode} #{new_resource.name} 
-     #{node[:hadoop][:home]}/bin/hadoop fs -chown #{new_resource.owner}:#{new_resource.group} #{new_resource.name} 
     EOF
   end
  
@@ -20,12 +20,12 @@ action :put do
 
   bash "hdfs-put-dir-#{new_resource.name}" do
     user #{new_resource.owner}
+    group #{new_resource.group}     
     code <<-EOF
      set -e
      . #{node[:hadoop][:home]}/sbin/set-env.sh
      #{node[:hadoop][:home]}/bin/hdfs dfs -put #{new_resource.name} #{new_resource.dest}
      #{node[:hadoop][:home]}/bin/hadoop fs -chmod #{new_resource.mode} #{new_resource.dest} 
-     #{node[:hadoop][:home]}/bin/hadoop fs -chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.dest} 
     EOF
     not_if "#{node[:hadoop][:home]}/bin/hadoop dfs -test -e #{new_resource.dest}"
   end
