4850e6815d2f26d452410b04be18339a0b2c4dd4
Jim Dowling <jdowling@kth.se>
2015-08-05 08:21:32 +0200

    services_spec now runs 2.7.1 MR pi job

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -28,7 +28,7 @@ describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -ls /\"") do
   its (:stdout) { should match /mr-history/ }
 end
 
-describe command("su yarn -l -c \"/srv/hadoop/bin/yarn jar /srv/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar pi 1 1000 \"") do
+describe command("su yarn -l -c \"/srv/hadoop/bin/yarn jar /srv/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar pi 1 1000 \"") do
 #  its(:exit_status) { should eq 0 }
   its (:stdout) { should match /Estimated value of Pi is/ }
 end
