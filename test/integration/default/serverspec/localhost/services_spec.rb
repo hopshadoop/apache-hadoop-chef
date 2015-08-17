96e87417101d957b25f4c58c5e052de62a3ee28d
Jim Dowling <jdowling@sics.se>
2015-08-17 22:29:55 +0200

    Fix setting up environment for format-nn command

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -28,8 +28,27 @@ describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -ls /\"") do
   its (:stdout) { should match /mr-history/ }
 end
 
-describe command("su yarn -l -c \"/srv/hadoop/bin/yarn jar /srv/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar pi 1 1000 \"") do
+describe command("su yarn -l -c \"/srv/hadoop/bin/yarn jar /srv/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar pi 1 1000 \"") do
 #  its(:exit_status) { should eq 0 }
   its (:stdout) { should match /Estimated value of Pi is/ }
 end
 
+describe command("service namenode restart") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("service datanode restart") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("service resourcemanager restart") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("service nodemanager restart") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("service historyserver restart") do
+  its(:exit_status) { should eq 0 }
+end
