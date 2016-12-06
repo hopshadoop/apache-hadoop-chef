bceb46be418989742ed31d1a3c3562fbce764241
Gautier Berthou <gautier.berthou@gmail.com>
2016-12-06 10:32:22 +0100

    add configuration of the capacity queue

--- attributes/default.rb
+++ attributes/default.rb
@@ -161,3 +161,17 @@ default.apache_hadoop.systemd                  = "true"
 
 default.apache_hadoop.log.maxfilesize          = "256MB"
 default.apache_hadoop.log.maxbackupindex       = 10
+
+#capacit scheduler queue configuration
+default.apache_hadoop.capacity.max_app                                  =10000
+default.apache_hadoop.capacity.max_am_percent                           =0.1
+default.apache_hadoop.capacity.resource_calculator_class                ="org.apache.hadoop.yarn.util.resource.DominantResourceCalculator"
+default.apache_hadoop.capacity.root.queues                              ="default"
+default.apache_hadoop.capacity.default_capacity                         =100
+default.apache_hadoop.capacity.user_limit_factor                        =1
+default.apache_hadoop.capacity.default_max_capacity                     =100
+default.apache_hadoop.capacity.default_state                            ="RUNNING"
+default.apache_hadoop.capacity.default_acl_submit_applications          ="*"
+default.apache_hadoop.capacity.default_acl_administer_queue             ="*"
+default.apache_hadoop.capacity.queue_mapping                            =""
+default.apache_hadoop.capacity.queue_mapping_override.enable            ="false"
