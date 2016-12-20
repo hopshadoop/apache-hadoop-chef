1fd465e5e31ee7163beb765a877473a33df3c3fe
Jim Dowling <jdowling@sics.se>
2016-12-20 10:56:41 +0100

    kagent added as an attr

--- metadata.rb
+++ metadata.rb
@@ -186,3 +186,6 @@ attribute "apache_hadoop/capacity/queue_mapping_override.enable",
           :description => "If a queue mapping is present, will it override the value specified by the user? This can be used by administrators to place jobs in queues that are different than the one specified by the user. The default is false.",
           :type => "string"
           
+attribute "kagent/enabled",
+          :description => "Set to 'true' to enable, 'false' to disable kagent",
+          :type => "string"
